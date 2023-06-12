d2a7442322f83bfbc9d3f0cbb1d649f00cffd789
lola <a@a.a>
2023-06-12 14:45:31 +0200

    Ajout du captcha

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Recaptcha\RecaptchaValidator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,13 +21,28 @@ class RegistrationController extends AbstractController
      */
 
     #[Route('/creer-un-compte/', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, RecaptchaValidator $recaptcha): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() ) {
+
+            // Récupération valeur du captcha
+            $captchaResponse = $request->request->get('g-recaptcha-response',  null);
+
+            // Récupération adresse IP
+            $ip = $request->server->get('REMOTE_ADDR');
+
+            // Si le captcha contient "null" ou n'est pas valide, on ajoute une erreur dans le formulaire
+            if($captchaResponse == null || $recaptcha->verify($captchaResponse, $ip)){
+                $form->addError( new FormError('Veuillez remplir le captcha de sécurité') );
+
+            }
+
+            if($form->isValid()) {
+
             // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -45,6 +62,8 @@ class RegistrationController extends AbstractController
 
             return $this->redirectToRoute('app_login');
         }
+    }
+
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
